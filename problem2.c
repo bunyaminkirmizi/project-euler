a6fc955d9da8cc8fb4e0053fd588cd8be772d4da
BUNYAMIN KIRMIZI <bunyamin.kirmizi@outlook.com>
2021-11-25 11:17:57 +0300

    problem two

---
+++ problem2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define LIMIT 4000000
+
+int main(){
+    int a = 1;
+    int b = 1;
+    int c = 0;
+    int sum = 0;
+
+    while(a < LIMIT){
+        c = a + b;
+        b = a + c;
+        sum +=c ;
+        a = b + c;
+    }
+
+    printf("ANSWER = %d",sum);
+}
